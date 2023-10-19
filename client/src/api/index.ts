71980db4fe28f8c9dd9ccde4a8b96f96ace84e06
mugishap <precieuxmugisha@gmail.com>
2023-10-19 17:28:18 +0200

    [fix]: Fix broken url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://healthtrack.onrender.com/api/v1'
+    baseURL: 'https://healthtrack-xmaw.onrender.com/api/v1'
 })
